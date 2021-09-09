6402e0f36362c1695c3f7ead6858b7126fd04f1a
Krishnendu Sadhu <43262321+krish2129@users.noreply.github.com>
2021-09-09 19:52:16 +0530

    update

--- src/index.js
+++ src/index.js
@@ -5,14 +5,10 @@ import { BrowserRouter as Router, HashRouter,Route, Switch } from "react-router-
 import Header from './components/header';
 import Home  from './components/home';
 
-
-
-
-
 import './css/index.css';
 
 //create the main class for displaying the recipes
-class PwcWeatherApi extends React.Component {
+class PwcWeatherApp extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -38,4 +34,4 @@ class PwcWeatherApi extends React.Component {
   }
 };
 
-ReactDOM.render(<PwcWeatherApi />, document.getElementById('app'));
+ReactDOM.render(<PwcWeatherApp />, document.getElementById('app'));
