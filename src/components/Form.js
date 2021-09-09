dffb027722f4322160634a7cf029109f946dc9c3
Krishnendu Sadhu <43262321+krish2129@users.noreply.github.com>
2021-09-09 21:27:40 +0530

    Update App Js

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,7 +3,7 @@ import React from "react";
 const Form = props => (
 	<form onSubmit={props.getWeather}>
 		<input type="text" name="city" placeholder="City..."/>
-		<select name="units" >
+		<select name="units" defaultValue={'metric'}>
 			<option value="metric" selected>Metric System</option>
 			<option value="imperial">Imperial System</option>			
     	</select>
