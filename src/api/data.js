d55d7b3a911b8b9c64912d843505977fa72c6c5e
Krishnendu Sadhu <43262321+krish2129@users.noreply.github.com>
2021-09-09 19:17:57 +0530

    Update 3

--- src/api/data.js
+++
@@ -1,8 +0,0 @@
-export default async function handler(req, res) {
-  const { input } = req.body;
-  const getWeatherData = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${input}&units=metric&appid=${process.env.API_KEY}`
-  );
-  const data = await getWeatherData.json();
-  res.status(200).json(data);
-}
